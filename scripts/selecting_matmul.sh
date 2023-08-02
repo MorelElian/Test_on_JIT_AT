7b3ba741bdde8390d8d0da9b1dd92cfc647a5ee5
MorelElian <elian.morel@laposte.net>
2023-08-02 05:37:57 -0400

    Adding the size of matrix as a parameter

--- scripts/selecting_matmul.sh
+++ scripts/selecting_matmul.sh
@@ -1,5 +1,11 @@
-clang++ -fjit -O3 Selecting_matmul.cpp -o selecting_matmul
-for ((i=1; i<=100; i++))
+#!/bin/bash
+clang++ -fjit -O3 ../Selecting_matmul.cpp -o ../bin/selecting_matmul
+echo -e "N;i;id_matmul;time_recorded\n" > ../data/trace_selecting_matmul.csv
+J="4 8 16 32 64 128 150 200 256"
+for j in $J
 do
-    ./selecting_matmul 10
+for i in $(seq 1 50)
+do
+    ../bin/selecting_matmul $j
+done
 done
